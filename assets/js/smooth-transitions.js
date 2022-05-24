1cf4e3006d94beccf37520788c730515f8028133
Aladdin <ninjalover249@gmail.com>
2022-05-24 10:51:34 -0300

    Add content to sections
    Add content to the different sections of the home page.

---
+++ assets/js/smooth-transitions.js
@@ -0,0 +1,30 @@
+/* === Smooth page transitions */
+let links = document.querySelectorAll('a');
+
+
+if (links) {
+  links.forEach(link => {
+    link.onclick = (e) => {
+      let body = document.querySelector('body');
+      e.preventDefault;
+
+      setTimeout(() => {
+        
+        if (body.classList.contains('fade-out')) {
+          console.log("navigating...");
+
+          if (!e.srcElement.parentElement.href) {
+            window.location = e.srcElement.href;
+          } else {
+            window.location = e.srcElement.parentElement.href;
+          }
+        } else {
+
+          console.log("whoops", e.srcElement);
+        }
+
+      }, 500);
+      body.classList.add('fade-out');
+    }
+  })
+}
