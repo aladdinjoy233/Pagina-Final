7547adc1e259d4827dbdd74d058a63d29a53a66e
Aladdin <ninjalover249@gmail.com>
2022-05-31 19:11:21 -0300

    Add sign-in validation
    Add validation for sign-in form and saving data to localStorage if all info is correct.

--- assets/js/validar-formulario.js
+++ assets/js/validar-formulario.js
@@ -9,12 +9,42 @@ function validarSign() {
   const errorDiv = document.querySelector('.error-msg');
   if (errorDiv) submitGroup.removeChild(errorDiv);
 
+  if (localStorage.getItem("email") && localStorage.getItem("contrasenia")) {
+    addEror("Ye tenes una cuenta en esta pagina")
+    return false;
+  }
+
   // Ver si los campos estan vacios
   if (formInputs.some(input => input.value === "")) {
     formInputs.forEach(input => input.value === "" ? input.classList.add("error-input") : null);
     addEror("No puede ver campos vacíos");
     return false;
   }
+
+  // Ver si el nombre y apellido son todos letras
+  if (hasNumbers(form.nombre.value.trim()) || hasNumbers(form.apellido.value.trim())) {
+    [form.nombre, form.apellido].forEach(input => hasNumbers(input.value.trim()) ? input.classList.add("error-input") : null);
+    addEror("Campo no puede tener numeros");
+    return false;
+  }
+
+  // Ver si email es valido
+  let emailReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!emailReg.test(form.email.value.trim())) {
+    form.email.classList.add("error-input");
+    addEror("Email no es valido");
+    return false;
+  }
+
+  // Ver si email es valido
+  let passReg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  if (!passReg.test(form.contrasenia.value.trim())) {
+    form.contrasenia.classList.add("error-input");
+    addEror("La contraseña debe tener como minimo 8 characteres, una letra y un numero");
+    return false;
+  }
+
+  signIn();
 
   return false;
 }
@@ -28,6 +58,18 @@ function addEror(message) {
   error.innerHTML += message;
 
   submitGroup.appendChild(error);
+}
+
+function hasNumbers(string) {
+  return /\d/.test(string);
+}
+
+function signIn() {
+  localStorage.setItem("nombre", form.nombre.value.trim());
+  localStorage.setItem("apellido", form.apellido.value.trim());
+  localStorage.setItem("email", form.email.value.trim());
+  localStorage.setItem("contrasenia", form.contrasenia.value.trim());
+  window.location.href = "../index.html";
 }
 
 // Ver contraseña
