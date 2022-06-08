8a4d1bf2151f9143677ee2a5437483411f616da0
Aladdin <ninjalover249@gmail.com>
2022-06-08 11:04:30 -0300

    Remove log-in form animation
    Remove span animation when clicking two different options, too buggy. Animation is now just CSS.

--- assets/js/log-in-highlight.js
+++ assets/js/log-in-highlight.js
@@ -1,18 +1,9 @@
-/* Crear un span que mueva con la opcion seleccionada */
 const triggerContainer = document.querySelector('section.forms-container');
 const triggers = document.querySelectorAll(".options > li");
 let activeLink = document.querySelector('.active-option');
 
-const highlight = document.createElement("span");
-highlight.classList.add("highlight");
-
-highlightLink(activeLink);
-document.body.append(highlight);
-
 // Event listeners for both btns
 triggers.forEach(trigger => trigger.addEventListener("click", () => {
-
-  highlightLink(trigger);
 
   if (!trigger.classList.contains("active-option")) {
     triggers.forEach(elem => elem.classList.toggle("active-option"));
@@ -31,21 +22,3 @@ triggers.forEach(trigger => trigger.addEventListener("click", () => {
   }
 
 }));
-
-window.addEventListener("resize", () => highlightLink(activeLink));
-
-// Highlight link function
-function highlightLink(elem) {
-  const linkCoords = elem.getBoundingClientRect();
-
-  coords = {
-    width: linkCoords.width,
-    height: linkCoords.height,
-    top: linkCoords.top - window.scrollY,
-    left: linkCoords.left - window.scrollX
-  }
-
-  highlight.style.width = `${coords.width}px`;
-  highlight.style.height = `${coords.height}px`;
-  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
-}
